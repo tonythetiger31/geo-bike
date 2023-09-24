b53b40498ca1f6aaa7e0078ade53cfe9149213c6
allierodrigues <albamaria20013@gmail.com>
2023-09-24 00:20:18 -0500

    made the map better

--- src/components/findBike.jsx
+++ src/components/findBike.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Link } from "react-router-dom";
 import { useState } from "react";
 
-const Profile = (props) => {
+const FindBike = (props) => {
 
     {/*Replace with real API fetch*/ }
     const bikesAvailable = 17;
@@ -28,27 +28,28 @@ const Profile = (props) => {
             <div className="profilePage">
                 <h1>Find a Bike</h1>
                 <div className="profileInfo">
-                    {/* <div className="profileFormat"> */}
+                    <div className="profileFormat">
                         <p>Choose Starting Point: </p>
-                        
-                        <iframe 
-                            src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d1732.5360409091445!2d-95.4021957129791!3d29.717671213487037!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sus!4v1695527572209!5m2!1sen!2sus" 
-                            width="100%" 
-                            height="450" 
+
+                        <iframe
+                            src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d1732.5360409091445!2d-95.4021957129791!3d29.717671213487037!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sus!4v1695527572209!5m2!1sen!2sus"
+                            width="100%"
+                            height="450"
                             style={{ border: '0' }}
-                            allowfullscreen="" 
-                            loading="lazy" 
+                            allowfullscreen=""
+                            loading="lazy"
                             referrerpolicy="no-referrer-when-downgrade"
                             title="Google Map"
 
                         ></iframe>
-                    {/* </div> */}
+                        <Link to="/profilesettings">
+                            <button className="editSettings">
+                                Edit Profile
+                            </button>
+                        </Link>
+                    </div>
                 </div>
-                <Link to="/profilesettings">
-                    <button className="editSettings">
-                        Edit Profile
-                    </button>
-                </Link>
+
 
 
 
@@ -57,4 +58,4 @@ const Profile = (props) => {
     );
 };
 
-export default Profile;
+export default FindBike;
--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -1,9 +1,7 @@
 import { useState } from "react";
-import Profile from "./findBike";
+import FindBike from "./findBike";
 import Account from "./accountinfo";
-// import QuoteForm from "./quoteform";
-import Quote from "./quote";
-import QuoteHistory from "./quotehistory";
+
 
 const Navigation = () => {
   const [component, setComponent] = useState(null);
@@ -17,7 +15,7 @@ const Navigation = () => {
       <div className="nav">
         <div className="nav2">
           <div className="nav-bar">
-            <button className="nav-button" onClick={() => setActiveCmponents(<Profile />)}>
+            <button className="nav-button" onClick={() => setActiveCmponents(<FindBike />)}>
               Find a bike
             </button>
             <button
